c1fd766af9b277c31a191aa285dab6aaf6d26da8
Piotr Turlej <piotr.turlej@me.com>
2018-10-22 20:58:51 +0200

    Small CreatePasswordHash refactor

--- Helpers/PasswordHash.cs
+++ Helpers/PasswordHash.cs
@@ -4,13 +4,13 @@ namespace UserAuth.API.Helpers
 {
     public static class PasswordHash
     {
-        public static Tuple<byte[], byte[]> CreatePasswordHash(string passsword, byte[] passwordHash, byte[] passswordSalt)
+        public static (byte[], byte[]) CreatePasswordHash(string passsword, byte[] passwordHash, byte[] passswordSalt)
         {
             using ( var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 passswordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passsword));
-                return Tuple.Create(passswordSalt, passwordHash);
+                return (passswordSalt, passwordHash);
             }
         }
 
