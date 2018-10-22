592e377b7b1644af1b7a966202c15695028bac91
Piotr Turlej <piotr.turlej@me.com>
2018-10-22 18:46:28 +0200

    TokenGenerator refactor and GetUsers improvements

--- Controllers/UserAuthController.cs
+++ Controllers/UserAuthController.cs
@@ -55,21 +55,7 @@ namespace UserAuth.API.Controllers
             try
             {
                 var user = await _authService.Login(userDto.Name, userDto.Password);
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Secret").Value);
-                var tokenDescriptor = new SecurityTokenDescriptor
-                {
-                   Subject = new ClaimsIdentity(new Claim[]
-                   {
-                        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                        new Claim(ClaimTypes.Name, user.Name)
-                    }),
-                    Expires = DateTime.Now.AddDays(1),
-                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
-                };
-                var token = tokenHandler.CreateToken(tokenDescriptor);
-                var tokenString = tokenHandler.WriteToken(token); 
-                return Ok(new {Token = tokenString });
+                return Ok(new {Token = new TokenGenerator().Generate(user, _config)});
 
             } catch (AppException ex)
             {
